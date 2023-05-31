5ca8dcdc8f645c12052f60e90be9fcdc6e55c5ec
Nirmitha-Kadaru <nirmithapriyadarshini@gmail.com>
2023-05-31 16:47:18 -0400

    kafka local sample

---
+++ src/main/java/com/project/constant/ApplicationConstant.java
@@ -0,0 +1,9 @@
+package com.project.constant;
+
+public class ApplicationConstant {
+	public static final String KAFKA_LOCAL_SERVER_CONFIG = "localhost:9092";
+	public static final String GROUP_ID_STRING = "group-id-test";
+	public static final String TOPIC_NAME = "sampleTopic";
+	public static final String KAFKA_LISTENER_CONTAINER_FACTORY = "kafkaListenerContainerFactory";
+	public static final String GROUP_ID_JSON = "group-id-testJson";
+}
